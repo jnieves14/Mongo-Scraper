7d4a7c3466b4c87ef44641eafdaecacf5188ad16
Jannah June Nieves <jannahjune@gmail.com>
2019-10-26 00:11:41 -0700

    jumbotron stretched

--- server.js
+++ server.js
@@ -41,7 +41,8 @@ app.set("view engine", "handlebars");
 mongoose.Promise = Promise;
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect(MONGODB_URI);mongoose.connect("mongodb://heroku_jmv816f9:5j1nd4taq42hi29bfm5hobeujd@ds133192.mlab.com:33192/heroku_jmv816f9");
+mongoose.connect(MONGODB_URI);
+// mongoose.connect("mongodb://heroku_jmv816f9:5j1nd4taq42hi29bfm5hobeujd@ds133192.mlab.com:33192/heroku_jmv816f9");
 //mongoose.connect("mongodb://localhost/mongoscraper");
 var db = mongoose.connection;
 
